d149807070ed0515919da8f077a2b4ca6a14376a
Marcelo <wxkzwin@gmail.com>
2019-08-31 14:15:24 -0300

    First push

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { FakeApiService } from '../../fake-api.service';
 
 @Component({
   selector: 'app-card',
@@ -7,18 +11,33 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  likeIcon: String = "like";
+
   @Input() card;
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
   }
 
   onLike(card: any){
-    // TODO: incrementar o like, salvar via rest
+    this.card.likes += 1;
+    var where = "/api/skills/"+this.card.id+"/likes/";
+    this.httpClient.post(where, {likes:this.card.likes}).subscribe(
+      // res => {
+      //     // console.log(res.valueOf())
+      // }
+  );
+    // this.httpClient.get('/api/skills').subscribe((ret: Array<any>) => console.log(ret));
+    if(this.card.likes == 5){
+      this.likeIcon = "like_blue"
+    }
+    if(this.card.likes == 10){
+      this.likeIcon = "like_purple"
+    }
   }
 
   onShare(card: any){
-    // TODO: abrir o link do seu linkedin
+    window.open("https://www.linkedin.com/in/marcelo-rodrigues-campos-5b338418a", "_blank");
   }
 
 }
